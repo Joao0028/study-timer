3224f9685f053dadb623ac8d54acf3194888e4a8
Joao0028 <joaopaulonascimento720@gmail.com>
2023-10-17 10:53:56 -0300

    criando função de conatr tempo e modificando o Item.tsx

--- src/components/Cronometro/Tempo/index.tsx
+++ src/components/Cronometro/Tempo/index.tsx
@@ -1,6 +1,10 @@
+import TempoParaSegundos from "../../../common/utils/Date";
+
 export default function Tempo() {
+  console.log("conversão: ", TempoParaSegundos("01:01:01"));
+  
   return (
-    <h2 className="text-[22px] mb-1 font-light max-sm:text-[20px]">
+    <h2 className="text-[22px] mb-1 font-extrabold max-sm:text-[20px]">
       00
       <span className="text-cor-azulClaro">:</span>
       00
--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -12,13 +12,12 @@ export default function Item({
   id,
   selecionaTarefa,
 }: Props) {
-  console.log("Item atual: ", { tarefa, tempo, selecionado, completado, id });
 
   return (
     <li
       className={` 
       ${selecionado ? "itemSelecionado" : ""} 
-      mb-3 bg-[#141414] pl-3 w-[325px] cursor-pointer py-[6px] border-solid border-b-2 border-cor-azulClaro max-md:w-[420px] max-[500px]:w-[320px] mr-1 `}
+      mb-3 bg-[#141414] pl-3 w-[325px] cursor-pointer py-[6px] border-solid border-b-2 ${selecionado? "": "border-cor-cinza"} max-md:w-[420px] max-[500px]:w-[320px] mr-1 `}
       onClick={() =>
         selecionaTarefa({
           tarefa,
